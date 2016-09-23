50b11a5071473556e4c6b5711a3fb43ba0a73235
jay <jay.wang.scorpion@gmail.com>
2016-09-23 16:49:16 +0800

    swiper :+1:

--- demo/pages/index/index.js
+++ demo/pages/index/index.js
@@ -3,8 +3,16 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World. This is Jay',
-    userInfo: {}
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 100,
+    items:["a","b","c","d","e","f","g","h","i","a","b","c","d","e","f","g","h","i"]
   },
   //事件处理函数
   bindViewTap: function() {
